393ca89961da0431b2d9e1745ef8d51e47ae7439
hiren3897 <59652404+hiren3897@users.noreply.github.com>
2020-02-04 20:17:37 +0100

    Add files via upload

---
+++ fetchData.java
@@ -0,0 +1,57 @@
+package fr.epita.hiren.dataModeling;
+
+public class fetchData {
+	String Name;
+	String Sex;
+	String height;
+	String age;
+	String weight;
+	
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getSex() {
+		return Sex;
+	}
+
+	public void setSex(String sex) {
+		Sex = sex;
+	}
+
+	public String getHeight() {
+		return height;
+	}
+
+	public void setHeight(String height) {
+		this.height = height;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getWeight() {
+		return weight;
+	}
+
+	public void setWeight(String weight) {
+		this.weight = weight;
+	}
+
+	public fetchData() {
+		
+	}
+	
+	
+	
+
+}
